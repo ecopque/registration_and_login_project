00f037175cf3a24d8cb1fb5e6892c58a6b473abd
Edson Copque <ecop@disroot.org>
2025-01-21 15:18:48 -0300

    Creating connection.

--- model.py
+++ model.py
@@ -1,11 +1,21 @@
 # FILE: /registration_and_login_project/model.py
 
-# pip install sqlalchemy
+# pip install sqlalchemy #8:
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Defining a database connection (previously): #9:
+USER = 'root'
+PASSWORD = '123456'
+HOST = 'localhost'
+DATABASE = 'registration_and_login'
+PORT = '3306'
 
+# Creating database through terminal (mariadb/mysql): #10:
+# CREATE DATABASE registration_and_login;
+
+CONN = f'mysql+pymysql//'
 
 
 
