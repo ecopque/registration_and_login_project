3e87539845519537eb1e0ef878cf7b6158d8fe31
Edson Copque <ecop@disroot.org>
2025-01-21 15:45:55 -0300

    Creating tables based on defined models.

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Defining a database connection (previously): #9:
+# Defining the database connection parameters: #9:
 USER = 'root'
 PASSWORD = '123456'
 HOST = 'localhost'
@@ -15,8 +15,26 @@ PORT = '3306'
 # Creating database through terminal (mariadb/mysql): #10:
 # CREATE DATABASE registration_and_login;
 
-CONN = f'mysql+pymysql//'
+# Creating the connection to the database: #11:
+CONN = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
+# Creating the SQLAlchemy engine and session: #12:
+engine = create_engine(CONN, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Defining the class model for the database table: #13:
+Base = declarative_base()
+
+# Creating tables in the database if they don't exist: #14:
+class Person(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    email = Column(String(30))
+    password = Column(String(500))
+
+# Creating tables based on defined models: #15:
+Base.metadata.create_all(engine)
 
 
 
