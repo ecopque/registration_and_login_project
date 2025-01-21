23c18e63e6f8471b634efa382c330732e9651acd
Edson Copque <ecop@disroot.org>
2025-01-21 11:54:31 -0300

    Defining database connection.

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
 # FILE: /registration_and_login_project/model.py
+
+# pip install sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+
+
+
+# Edson Copque | https://linktr.ee/edsoncopque
