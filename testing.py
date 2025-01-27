df90a93cfbb4f45fe35d3654df7ddf8aa013ca90
Edson Copque <ecop@disroot.org>
2025-01-27 14:40:22 -0300

    Testing...

--- testing.py
+++ testing.py
@@ -5,84 +5,75 @@
 
 #####################################################
 # model.py
-
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
 
-# Informar dados de conexão:
-USER = 'root'
+USER = 'ROOT'
 PASSWORD = '123456'
 HOST = 'localhost'
 PORT = '3306'
 DATABASE = 'registration_and_login'
 
-# Variável de conexão:
 CONN = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
-# Criando engine e session:
+# CREATE DATABASE registration_and_login;
+
 engine = create_engine(CONN, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Criando a base da tabela:
 Base = declarative_base()
 
-# Defindo tabela [se não existir]:
 class Person(Base):
     __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
-    name = Column(String(100))
+    name = Column(String(50))
     email = Column(String(100))
     password = Column(String(500))
 
-# Executando tabela:
 Base.metadata.create_all(engine)
 
-#####################################################
-# controller.py (Registro, Login, Remove)
+# controller:
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model import Person
 import hashlib
 
-# Definindo sessão/retorno/conexão:
 def return_session():
     USER = 'root'
+    USER = 'ROOT'
     PASSWORD = '123456'
     HOST = 'localhost'
-    DATABASE = 'registration_and_login'
     PORT = '3306'
+    DATABASE = 'registration_and_login'
 
-    # Definindo variável de conexão:
     CONN = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
-    # Definindo engine e session:
     engine = create_engine(CONN, echo=True)
-    Session = session(bind=engine)
+    Session = sessionmaker(bind=engine)
     return Session()
 
 class RegisterController():
     @classmethod
     def check_data(cls, ck_name, ck_email, ck_password):
         if len(ck_name) > 50 or len(ck_name) < 3:
-            return 'aaa'
-        
-        if len(ck_email) > 30:
-            return 'bbb'
-
+            return 'Nome inválido.'
+        if len(ck_email) > 100:
+            return 'E-mail inválido.'
         if len(ck_password) > 500 or len(ck_password) < 6:
-            return 'ccc'
-        
+            return 'Password inválido.'
         else:
             return 'Data is valid.'
-
+    
     @classmethod
     def register(cls, rg_name, rg_email, rg_password):
         session = return_session()
 
-        user_check = session.query(Person).filter(Person.email == rg_email).all()
-        if len(user_check) > 0:
+        user = session.query(Person).filter(Person.email == rg_email).first()
+        if len(user) > 0:
             return 'E-mail já existe.'
         
         valid_data = cls.check_data(rg_name, rg_email, rg_password)
@@ -90,26 +81,11 @@ class RegisterController():
             return valid_data
 
         try:
-            password_hash = hashlib.sha256(rg_password.encode()).hexdigest()
-            new_user = Person(name=rg_name, email=rg_email, password=password_hash)
+            hash_password = hashlib.sha256(rg_password.encode()).hexdigest()
+            new_user = Person(name=rg_name, email=rg_email, password=hash_password)
             session.add(new_user)
             session.commit()
-            return 'Usuário registrado com sucesso.'
-
+            return 'Registro realizado com sucesso.'
+        
         except Exception as error:
-            print(f'Error:{error}')
-
-
-class LoginController():
-    @classmethod
-    def login(cls, lg_email, lg_password):
-        session = return_session()
-
-        lg_password = hashlib.sha256(lg_password.encode()).hexdigest()
-        logged_check = session.query(Person).filter(Person.email == lg_email).filter(Person.password == lg_password).first()
-
-        if len(logged_check) > 0:
-            return f'Login efetuado. {logged_check[0].id}'
-
-        else:
-            return 'E-mail ou senha errados.'
+            print(f'Error: {error}')
